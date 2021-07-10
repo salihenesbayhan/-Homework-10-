f127e7dece34f3c8393c6ce8de2db559a4c000ee
salihenesbayhan <80679472+salihenesbayhan@users.noreply.github.com>
2021-07-10 10:00:37 +0300

    Add files via upload

---
+++ pyAesCrypt/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+setup(name='pyAesCrypt',
+    version='6.0.0',
+    packages = find_packages(),
+    include_package_data=True,
+    description='Encrypt and decrypt files and streams in AES Crypt format (version 2)',
+    long_description = README,
+    author='Marco Bellaccini',
+    url='https://github.com/marcobellaccini/pyAesCrypt',
+    license='Apache License 2.0',
+    scripts=['bin/pyAesCrypt'],
+    install_requires=['cryptography'],
+    keywords = "AES Crypt encrypt decrypt",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Utilities',
+    ],
+)
